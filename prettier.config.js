a7f8c8a2285e6c633941511c790604984825f933
Hitarth Sharma <hitarth.sharma@outlook.com>
2023-12-04 10:42:44 +1300

    setup styling, add fonts and routes

--- prettier.config.js
+++ prettier.config.js
@@ -2,5 +2,13 @@ const styleguide = require('@vercel/style-guide/prettier');
 
 module.exports = {
   ...styleguide,
+  ...{
+    "semi": true,
+    "trailingComma": "all",
+    "singleQuote": true,
+    "printWidth": 80,
+    "tabWidth": 2,
+    "useTabs": false,
+  },
   plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
 };
